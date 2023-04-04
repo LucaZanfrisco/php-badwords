5963aa0ba75da0c2bbd6f6c89e73738dc13e2eeb
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-04-04 16:33:18 +0200

    first commit

---
+++ index1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+</head>
+<body>
+    <form action="index2.php" methods="GET">
+        <textarea name="words" id="words" cols="50" rows="1"></textarea>
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
